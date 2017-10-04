b909b7d310cc9ee41ec86ed50bb58ccd5cc89c40
camila <galesmrea@gmail.com>
2017-10-04 18:24:08 -0500

    commit inicial

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express ();
+
+app.configure (function() {
+  app.use(express.bodyparser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+app.get('/',function(req,res){
+  res.send('hola, mundo');
+});
+app.listen(5000);
+console.log('servidor express escuchando en el puerto 5000');
