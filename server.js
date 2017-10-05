a0a6abb1c2c317459f1c28a1c945a8d6e95b5716
juanpabloramirez-wk <juanpablo.ramirez@workiva.com>
2017-10-04 22:43:29 -0500

    mongodb working

--- server.js
+++ server.js
@@ -2,15 +2,26 @@ var express = require('express');
 var mongoose = require('mongoose');
 var app = express ();
 
-app.configure (function() {
-  app.use(express.bodyparser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-});
-app.get('/',function(req,res){
-  res.send('hola, mundo');
-});
-app.listen(5000);
-console.log('servidor express escuchando en el puerto 5000');
-#JUAN PABLO es mi compai
-   
+var db = mongoose.connect("mongodb://multiayudavital:multiayudavital1@ds111535.mlab.com:11535/multiayudavital",
+  {useMongoClient: true,}
+);
+
+var exports = module.exports = {};
+
+app.get('/', function (req, res) {
+  res.send('Hola Mundo')
+})
+
+
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
+  console.log('Example app listening on ${port} 3000!')
+})
+
+exports.closeServer = function(){
+  server.close();
+  db.disconnect();
+  console.log('bye')
+};
+//JUAN PABLO es mi compai
