6c47d96c30e269fd336ebadbeccff301d74add7c
camila <galesmrea@gmail.com>
2017-10-26 00:03:17 -0500

    obtener parametros desde la url

--- server.js
+++ server.js
@@ -12,27 +12,26 @@ app.get('/', function (req, res) {
   res.send('Hola Mundo')
 })
 
-
-
 app.get('/usuarioMobil', function (req, res) {
-  res.send('Hola Mundo usuarioMobil')
+  console.log('Este es el nombre de usuario: ' + req.query.nombreUsuario);
+console.log('Este es el tipo de emergencia: ' + req.query.tipoEmergencia);
+console.log('Esta es la ubicacion: ' + req.query.ubicacion);
+  res.send('Hola esta es una prueba');
 })
 
 app.get('/usuarioWeb', function (req, res) {
-  res.send('Hola Mundo usuarioWeb')
+  res.send('Hola Mundo usuarioWeb');
 })
-
-
 
 var port = process.env.PORT || 3000;
 
 var server = app.listen(port, function () {
-  console.log(`Example app listening on ${port}!`)
+  console.log(`Example app listening on ${port}!`);
 })
 
 exports.closeServer = function(){
   server.close();
   db.disconnect();
-  console.log('bye')
+  console.log('bye');
 };
 //JUAN PABLO es mi compai
