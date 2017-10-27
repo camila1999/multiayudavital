c08613ec6f33946d9e5754a6d6d4eddeda2f26ab
juanpabloramirez-wk <juanpablo.ramirez@workiva.com>
2017-10-26 22:36:27 -0500

    sync master

--- jajajaja.js
+++ jajajaja.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var moment = require('moment');
 var app = express ();
 
 var db = mongoose.connect("mongodb://multiayudavital_web_app1:multiayudavital1@ds111535.mlab.com:11535/multiayudavital",
@@ -8,8 +9,8 @@ var db = mongoose.connect("mongodb://multiayudavital_web_app1:multiayudavital1@d
 db.on('error', console.error.bind(console, 'connection error:'));
 var usuariosEmergenciasSchema = mongoose.Schema({
 	fecha: String,
-  usuario: String,
-	tipo_emergencia: String,
+    nombreUsuario: String,
+	tipoDeEmergencia: String,
 	ubicacion: String
 });
 var usuariosEmergenciasModel = mongoose.model('Usuarios_Emergencias',usuariosEmergenciasSchema);
@@ -20,30 +21,20 @@ app.get('/', function (req, res) {
   res.send('Hola Mundo')
 })
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/index', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-})
-
 app.get('/usuarioMobil', function (req, res) {
-  console.log('Este es el nombre de usuario: ' + req.query.usuario);
-  console.log('Este es el tipo de emergencia: ' + req.query.tipo_emergencia);
+  console.log('Este es el nombre de usuario: ' + req.query.nombreUsuario);
+  console.log('Este es el tipo de emergencia: ' + req.query.tipoDeEmergencia);
   console.log('Esta es la ubicacion: ' + req.query.ubicacion);
 
-  var fecha = Date.now();
-  var usuario = req.query.usuario;
-  var tipo_emergencia = req.query.tipo_emergencia;
+  var fecha = moment().format();
+  var nombreUsuario = req.query.nombreUsuario;
+  var tipoDeEmergencia = req.query.tipoDeEmergencia;
   var ubicacion = req.query.ubicacion;
 
   var datosAInsertar = new usuariosEmergenciasModel({
 	  fecha: fecha,
-    usuario: usuario,
-	  tipo_emergencia: tipo_emergencia,
+      nombreUsuario: nombreUsuario,
+	  tipoDeEmergencia: tipoDeEmergencia,
 	  ubicacion: ubicacion
   });
   datosAInsertar.save();
