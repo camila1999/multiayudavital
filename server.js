690ab9ba45b5e17939eb9f0b7d3c2d0c18b4bb33
juanpabloramirez-wk <juanpablo.ramirez@workiva.com>
2017-10-27 01:39:23 -0500

    fix branch

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/usuarioMobil', function (req, res) {
 	  ubicacion: JSON.parse(ubicacion)
   });
   datosAInsertar.save();
-    res.render('exito');
+    res.render('exito.html');
 
 })
 
